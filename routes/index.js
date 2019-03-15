b9a738da0bb712856bcdddf62585a11982abfa1a
shane mc donagh <shane.mc.donagh24693@gmail.com>
2019-03-15 01:38:29 +0000

    Added Home & Menu page (shoping Cart Started)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/Menu');
+var Schema = mongoose.Schema;
 
-/* GET home page. */
+var userDataSchema = new Schema({
+  Type: String,
+  Name: String,
+  Discription: String,
+  Image: String
+});
+
+var UserData = mongoose.model('menuitems', userDataSchema);
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  UserData.find()
+      .then(function(doc) {
+        console.log("Got as far as here, array length is: " + doc.length);
+        res.render('index', {title: "My Page", items: doc});
+        console.log(doc);
+      });
+});
+router.get('/products', function(req, res, next) {
+  UserData.find()
+      .then(function(doc) {
+        console.log("Got as far as here, array length is: " + doc.length);
+        res.render('products', {title: "My Page", items: doc});
+        console.log(doc);
+      });
 });
 
 module.exports = router;
